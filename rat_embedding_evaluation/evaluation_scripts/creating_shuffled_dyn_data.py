3dea798d86fc2aeafb4db9df49444aa8cde18dbb
Akshey <akshey.kumar@univie.ac.at>
2024-09-06 16:30:26 +0200

    creating baseline with linear deterministic dynamics for dyanmics evluations

--- rat_embedding_evaluation/evaluation_scripts/creating_shuffled_dyn_data.py
+++ rat_embedding_evaluation/evaluation_scripts/creating_shuffled_dyn_data.py
@@ -4,9 +4,12 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
 
 # creating shuffled data
 for rat_name in ['achilles', 'gatsby', 'cicero', 'buddy']:
+    """
     algorithm = 'BunDLeNet'
     print(algorithm, ' rat_name: ', rat_name)
 
@@ -49,3 +52,35 @@ for rat_name in ['achilles', 'gatsby', 'cicero', 'buddy']:
     np.savetxt(file_pattern.format('y1_tst'), y1_tst_point)
     np.savetxt(file_pattern.format('b_train_1'), b_train_1)
     np.savetxt(file_pattern.format('b_test_1'), b_test_1)
+    """
+    algorithm = 'linear_dynamics'
+    def linear_dynamics_generator(sequence_length):
+        theta = 0.01
+        T_Y = np.array([[np.cos(theta)*np.cos(3*theta), - np.sin(theta), - np.sin(3*theta)],
+                      [np.sin(theta), np.cos(theta) , 0],
+                      [np.sin(3*theta), 0, np.cos(3*theta)]])
+        y_0 = np.array([1, -1, 0.5])
+        y = [y_0]
+        for i in range(sequence_length):
+            y.append( T_Y @ y[-1] )
+        return np.array(y)
+
+
+    y0_tr = np.zeros((3232,3))
+    y1_tr = np.zeros((3232,3))
+    y0_tst = np.zeros((3232,3))
+    y1_tst = np.zeros((3232,3))
+
+    y0_tr_linear = linear_dynamics_generator(y0_tr.shape[0])
+    y1_tr_linear = linear_dynamics_generator(y1_tr.shape[0])
+    y0_tst_linear = linear_dynamics_generator(y0_tst.shape[0])
+    y1_tst_linear = linear_dynamics_generator(y1_tst.shape[0])
+
+    file_pattern = f'data/generated/saved_Y/{{}}__{algorithm}_rat_{rat_name}'
+    np.savetxt(file_pattern.format('y0_tr'), y0_tr_linear)
+    np.savetxt(file_pattern.format('y1_tr'), y1_tr_linear)
+    np.savetxt(file_pattern.format('y0_tst'), y0_tst_linear)
+    np.savetxt(file_pattern.format('y1_tst'), y1_tst_linear)
+    np.savetxt(file_pattern.format('b_train_1'), b_train_1)
+    np.savetxt(file_pattern.format('b_test_1'), b_test_1)
+
