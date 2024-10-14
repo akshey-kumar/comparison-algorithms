c5e3b1e3396881dccd25005df930f4ed88bba26f
Akshey <akshey.kumar@univie.ac.at>
2024-10-14 17:29:50 +0200

    further evaluation

--- c_elegans_embedding_evaluation/evaluation_scripts/run_evaluations.sh
+++ c_elegans_embedding_evaluation/evaluation_scripts/run_evaluations.sh
@@ -11,9 +11,9 @@
 # Loop for behaviour_decoding_analysis.py and dynamics_predictability.py
 for worm_num in 0 1 2 3 4
 do
-    for algorithm in 'BunDLeNet_win_1' # 'PCA' 'tsne' 'autoencoder' 'ArAe' 'BunDLeNet' 'cebra_h' # 'PCA_time_delay_embedding' 'autoencoder_time_delay_embedding' 'ArAe_time_delay_embedding' 'tsne_time_delay_embedding' 'cebra_h_time_delay_embedding' 'LDA_time_delay_embedding' 'BunDLeNet_linear'
+    for algorithm in 'PCA' 'tsne' 'autoencoder' 'ArAe' 'BunDLeNet' 'cebra_h' # 'PCA_time_delay_embedding' 'autoencoder_time_delay_embedding' 'ArAe_time_delay_embedding' 'tsne_time_delay_embedding' 'cebra_h_time_delay_embedding' 'LDA_time_delay_embedding' 'BunDLeNet_linear' 'BunDLeNet_win_1'
     do
-        python3 c_elegans_embedding_evaluation/evaluation_scripts/behaviour_decoding.py $algorithm $worm_num
+        # python3 c_elegans_embedding_evaluation/evaluation_scripts/behaviour_decoding.py $algorithm $worm_num
         python3 c_elegans_embedding_evaluation/evaluation_scripts/dynamics_predictability.py $algorithm $worm_num
     done
 done
